cdf2ae8a3db248db1b35cf30698cd8b0b8490e4c
lschelling <lorenz.schelling@gmx.net>
2024-01-17 09:48:04 +0100

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,84 +1,129 @@
-from enum import Enum 
 import pgzrun as pgz
+from pygame import Rect
 import random
 
-BOARD_SIZE = 30
+
+BOARD_SIZE = 16
 SEGMENT_SIZE = 20
-WIDTH = 600
-HEIGHT = 600
+WIDTH = BOARD_SIZE * SEGMENT_SIZE
+HEIGHT = BOARD_SIZE * SEGMENT_SIZE
 WHITE = 255, 255, 255
+GREEN = 0,255,0
+RED = 255,0,0
+GREY= 100,100,100
 
-class Direction(Enum):
-    E = 1
-    N = 2
-    W = 3
-    S = 4
-    X = 5
-
+class Apple :
+    def __init__(self):
+        self.new_apple()
+        
+    def new_apple(self):
+        self.ypos=random.randint(2,BOARD_SIZE-2)
+        self.xpos=random.randint(1,BOARD_SIZE-2)        
+    
+    def draw(self,screen) :
+        r =Rect (self.xpos*SEGMENT_SIZE, self.ypos*SEGMENT_SIZE, SEGMENT_SIZE, SEGMENT_SIZE) 
+        screen.draw.filled_rect(r,RED)
+            
+                
 class Segment:
-    def __init__(self, posx, posy):
-        self.xpos = posx
-        self.ypos = posy
+     def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
 
 class Snake:
-    def __init__(self, posx, posy):
-        self.segments = []
-        self.segments.append(Segment(posx, posy))
-        self.segments.append(Segment(posx+1, posy))
-        self.segments.append(Segment(posx+2, posy))
-        self.grow = False
-        
-    def move(self, direction):
-        dy=dx=0
-        if direction == Direction.E:
-            dx=1
-        elif direction == Direction.N:
-            dy=-1
-        if direction == Direction.W:
-            dx=-1
-        elif direction == Direction.S:
-            dy=1
-
-        self.segments.insert(0, Segment(self.segments[0].xpos+dx, self.segments[0].ypos+dy))
-
-        if self.grow:
-            self.grow=False
-        else:
-            self.segments.pop()        
-
-    def draw(self, screen):
-        for s in self.segments:
-            seg = Rect (s.xpos * SEGMENT_SIZE, s.ypos * SEGMENT_SIZE, SEGMENT_SIZE, SEGMENT_SIZE)
-            screen.draw.filled_rect(seg, WHITE)
-
-    def eat(self):
-        self.grow = True
-
-snake = Snake(15,15)
-direction = Direction(Direction.X)
-
-def snakemove():
-    global direction
-    snake.move(direction)
     
-clock.schedule_interval(snakemove, 0.5)
+    def __init__(self, board_size, segment_size):
+        self.segment_size=segment_size
+        self.board_size=board_size
+        self.segments=[]
+        self.bricks=[]
+        self.apples=[]
+        self.setup_snake()
+        self.setup_walls()
+        self.setup_apples()
+        self.score=0
+        self.gameover=False
+        self.collect_done = 3
+        self.cframe= 0
+        self.speed= 20
 
-def draw():
-    screen.clear()
-    snake.draw(screen)
 
-def update():
-    global direction
-    if keyboard.right:
-        direction = Direction.E
-    elif keyboard.left:
-        direction = Direction.W
-    if keyboard.down:
-        direction = Direction.S
-    elif keyboard.up:
-        direction = Direction.N        
+    def setup_apples(self):
+        self.apples.append(Apple())
+    
+    def setup_snake(self):
+        self.segments.append(Segment(self.board_size//2,self.board_size//2))
+        self.segments.append(Segment(self.board_size//2,self.board_size//2+1))
+        self.segments.append(Segment(self.board_size//2,self.board_size//2+2))
 
-    if keyboard.space:
-        snake.eat()
+    def setup_walls(self):
+        for x in range(0,BOARD_SIZE ):
+            self.bricks.append(Segment(x,1))
+            self.bricks.append(Segment(x,self.board_size-1))
+        for y in range(2,BOARD_SIZE-1):
+            self.bricks.append(Segment(0,y))
+            self.bricks.append(Segment(self.board_size-1,y))
+            
+    def get_level_text(self):
+        return 'Collect ' + str(self.collect_done) + ' apples'
+    
+    def draw(self,screen, level) :
 
-pgz.go()
+        h = True
+        for s in self.segments:
+            r =Rect (s.xpos*self.segment_size, s.ypos*self.segment_size, self.segment_size, self.segment_size) 
+            if h :
+                screen.draw.filled_rect(r,WHITE)
+                h = False
+            else :
+                screen.draw.filled_rect(r,GREEN)
+        for b in self.bricks:
+            r =Rect (b.xpos*self.segment_size, b.ypos*self.segment_size, self.segment_size, self.segment_size) 
+            screen.draw.filled_rect(r,GREY)
+        self.apples[0].draw(screen)
+        screen.draw.text('score__'+str(self.score),(0,0),color=(0,255,255))
+        screen.draw.text('level__'+str(level),(self.board_size*self.segment_size-120,0),color=(0,255,255))
+        
+    def check_gameover(self,x,y):
+        for s in self.segments:
+            if x== s.xpos and y== s.ypos:
+                return True
+        for b in self.bricks:
+            if x== b.xpos and y== b.ypos:
+                return True
+        return False
+        
+
+    def level_done(self):
+        return self.score >= self.collect_done
+        
+    def update(self,richtung):
+        self.cframe+=1
+        if self.cframe % self.speed == 0:
+            x = self.segments[0].xpos
+            y = self.segments[0].ypos
+            if richtung =='N':
+                y = y+1
+            elif richtung =='E':
+                x = x+1
+            elif richtung =='W':
+                x= x-1
+            elif richtung =='S':
+                y= y-1
+            else:
+                return
+
+            if False==self.check_gameover(x,y) :
+                self.segments.insert(0,Segment(x,y))
+                if self.apples[0].xpos==x and self.apples[0].ypos==y :
+                    self.apples[0]=Apple()
+                    self.score+=1
+                else:
+                    self.segments.pop()
+                    
+            else : 
+                self.gameover = True
+
+        if self.cframe % (60*2) == 0 and self.speed > 2:
+            self.speed-=1
+
